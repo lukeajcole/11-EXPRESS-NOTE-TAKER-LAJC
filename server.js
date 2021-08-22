526300a870e60ad0226784b8a1cf57b204c67dd9
Luke Cole <Lukeajcole@gmail.com>
2021-08-21 19:01:02 -0700

    Fix Port for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require(path.join(__dirname, '/db/db.json'));
 const fs = require('fs');
 
 const app = express();
-const PORT =  3001;
+const PORT =  process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
